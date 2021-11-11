7117bf04ea12f18a3f7ea1677593df021d2330fd
hafizahmed2 <hafizahmed.tx@gmail.com>
2021-11-11 21:18:47 +0500

    Added remaining popups and made some design tweaks

--- app/javascript/bbr/modals.js
+++ app/javascript/bbr/modals.js
@@ -1,7 +1,14 @@
 $(document).on('turbolinks:load', function() {
   if($('[data-modal="close"]').length) {
-    $('[data-modal="close"]').on('click', () => {
-      $('#bbr-modal').hide();
+    $('[data-modal="close"]').on('click', function() {
+      $(this).parents('.is-modal').hide()
+    });
+  }
+
+  if($('[data-toggle="modal"]').length) {
+    $('[data-toggle="modal"]').on('click', function(event){
+      $($(this).data('target')).show();
+      event.preventDefault();
     });
   }
 });
