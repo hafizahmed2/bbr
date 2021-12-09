240e2cf872b548ebcdfa5a72086dab982d613fb3
hafizahmed2 <hafizahmed.tx@gmail.com>
2021-12-09 11:27:38 -0800

    Added FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :experiences, only: [:new, :create]
   resources :project_supports, only: [:new, :create]
   resources :bookings, only: [:new, :create]
+  resources :faqs, only: [:index]
 
   root to: 'home#index'
 end
